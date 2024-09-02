bde6fcc2ac0174bd97de816eb974b3964c8ad114
mapsandapps <shawn@mapsandapps.net>
2024-09-02 11:08:53 -0400

    Center calendar

--- src/components/Month.tsx
+++ src/components/Month.tsx
@@ -22,22 +22,24 @@ export default function Month(props: MonthProps) {
   }
 
   return (
-    // modified from https://www.reddit.com/r/svg/comments/1e3qehg/svg_animation_calendar_like/
-    <svg className="month-svg" xmlns='http://www.w3.org/2000/svg' viewBox='0 0 128 96' width='128' height='96'>
-      <line className="calendar-bar" x1='0' x2='128' y1='0' y2='0' />
-      
-      <g className='pages'>
-        <g className='page new-month'>
-          <rect x='4' y='8' />
-          <text x='64' y='56'>{ currentMonthName }</text>
+    <div className="month">
+      {/* modified from https://www.reddit.com/r/svg/comments/1e3qehg/svg_animation_calendar_like/ */}
+      <svg className="month-svg" xmlns='http://www.w3.org/2000/svg' viewBox='0 0 128 96' width='128' height='96'>
+        <line className="calendar-bar" x1='0' x2='128' y1='0' y2='0' />
+        
+        <g className='pages'>
+          <g className='page new-month'>
+            <rect x='4' y='8' />
+            <text x='64' y='56'>{ currentMonthName }</text>
+          </g>
+          <g className='page old-month'>
+            <rect x='4' y='8' />
+            <text x='64' y='56'>{ prevMonthName }</text>
+          </g>
         </g>
-        <g className='page old-month'>
-          <rect x='32' y='64' />
-          <text x='64' y='56'>{ prevMonthName }</text>
-        </g>
-      </g>
 
-      <line className="calendar-perforate" x1='4' x2='124' y1='8' y2='8' />
-    </svg>
+        <line className="calendar-perforate" x1='4' x2='124' y1='8' y2='8' />
+      </svg>
+    </div>
   )
 }
